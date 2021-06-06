3dafb2d7c87c57c6c3cb84d6b793f0ea8623d63d
Kevin <kevin.lam13@hotmail.com>
2021-06-05 18:40:18 -0600

    first commit

---
+++ src/hooks/useCalculadora.tsx
@@ -0,0 +1,147 @@
+import { useRef, useState } from "react";
+
+enum Operadores {
+    sumar, 
+    restar, 
+    multiplicar, 
+    dividir
+}
+
+export const useCalculadora = () => {
+    const [numeroAnterior, setNumeroAnterior] = useState('0');
+    const [numero, setNumero] = useState('0');
+
+    const ultimaOperacion = useRef<Operadores>();
+
+    const limpiar = () => {
+        setNumero('0');
+        setNumeroAnterior('0');
+    }
+
+    const armarNumero = (numeroTexto: string) => {
+
+        //No aceptar doble punto
+        if(numero.includes('.') && numeroTexto === '.') return;
+
+        if(numero.startsWith('0') || numero.startsWith('-0')){
+            
+            // Punto decimal
+            if(numeroTexto === '.'){
+                setNumero(numero + numeroTexto);
+                
+                //Evaluar si es otro cero y hay un punto
+            } else if(numeroTexto === '0' && numero.includes('.') ){
+                setNumero(numero + numeroTexto);
+                
+                // Evaluar si es diferente de cero y no tiene punto
+            } else if (numero == '0' && !numero.includes('.')) {
+                setNumero(numeroTexto);
+
+                //Evitar 0000.0
+            } else if (numeroTexto === '0' && !numero.includes('.')){
+                setNumero(numero);
+            } else {
+                setNumero(numero + numeroTexto);
+            }
+
+        }else{
+            setNumero(numero + numeroTexto);
+        }
+    }
+
+    const positivoNegativo = () => {
+        if(numero.includes('-')){
+            setNumero(numero.replace('-', ''));
+        }else {
+            setNumero('-'+numero);
+        }
+    }
+
+    const btnDelete = () => {
+        let negativo = '';
+        let temp = numero;
+
+        if(numero.includes('-')){
+            negativo = '-';
+            temp =  numero.substr(1);
+        }
+
+        if(numero.length > 1){
+            setNumero( negativo + temp.slice(0, -1));
+        } else{
+            setNumero('0');
+        }
+    }
+
+    const cambiarNumeroPorAnterior = () => {
+        if(numero.endsWith('.')){
+            setNumeroAnterior(numero.slice(0, -1));
+        }else{
+            setNumeroAnterior(numero);
+        }
+        setNumero('0');
+    }
+
+    const btnDividir = () => {
+        cambiarNumeroPorAnterior();
+        ultimaOperacion.current = Operadores.dividir;
+    }
+
+    const btnMultiplicar = () => {
+        cambiarNumeroPorAnterior();
+        ultimaOperacion.current = Operadores.multiplicar;
+    }
+
+    const btnRestar = () => {
+        cambiarNumeroPorAnterior();
+        ultimaOperacion.current = Operadores.restar;
+    }
+
+    const btnSumar = () => {
+        cambiarNumeroPorAnterior();
+        ultimaOperacion.current = Operadores.sumar;
+    }
+
+    const calcular = () => {
+        const num1 = Number(numero);
+        const num2 = Number(numeroAnterior);
+
+        if(isNaN(num1)) return;
+        
+        switch(ultimaOperacion.current){
+            case Operadores.sumar:
+                setNumero(`${num1+num2}`);
+                break;
+
+            case Operadores.restar:
+                 setNumero(`${num1-num2}`);
+                break;
+
+            case Operadores.multiplicar:
+                setNumero(`${num1*num2}`);
+                break;
+            
+            case Operadores.dividir:
+                setNumero(`${num2/num1}`);
+                break;
+        }
+
+        setNumeroAnterior('0');
+
+    }
+
+    return {
+        numero,
+        numeroAnterior,
+        limpiar,
+        armarNumero,
+        positivoNegativo,
+        btnDelete,
+        cambiarNumeroPorAnterior,
+        btnSumar,
+        btnRestar,
+        btnMultiplicar,
+        btnDividir,
+        calcular
+    };
+}
